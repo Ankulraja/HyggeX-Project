ea672257d3eb03537ff12f40ad66f0676b4bca2d
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-05-10 00:55:23 +0530

    Fifth Change

--- src/Components/Core/FlashCard/View/Study.js
+++ src/Components/Core/FlashCard/View/Study.js
@@ -7,10 +7,12 @@ import ReactPlayer from "react-player";
 import { HiMiniSpeakerWave } from "react-icons/hi2";
 import { useEffect, useRef, useState } from "react";
 import { IoVolumeMuteSharp } from "react-icons/io5";
+
 const Study = () => {
   const [page, setPage] = useState(0);
   const videoRef = useRef(null);
   const [mute, setMute] = useState(false);
+
   const incrementor = () => {
     if (page + 1 < VideoLink?.length) {
       setPage(page + 1);
@@ -32,42 +34,41 @@ const Study = () => {
   const muteHandler = () => {
     setMute((prevMute) => !prevMute);
   };
-  
+
   const fullScreenHandler = () => {
     if (videoRef.current) {
       videoRef.current.wrapper?.requestFullscreen();
     }
   };
+
   useEffect(() => {}, [mute]);
+
   return (
-    <div className="w-full  mx-auto py-5 flex flex-col justify-center items-center">
-      <div className="w-6/12 max-lg:w-7/12 max-md:w-11/12 auto">
-        <div className="w-full relative mx-auto flex justify-center items-center group">
-          <div className="border">
+    <div className="w-full mx-auto py-5 flex flex-col justify-center items-center">
+      <div className="w-full md:w-7/12 lg:w-9/12">
+        <div className="relative mx-auto flex justify-center items-center group">
           <ReactPlayer
             ref={videoRef}
             url={VideoLink[page].url}
-            width={"500px"}
-            height={"290px"}
+            width="500px"
+            height="320px"
             playing={true}
             muted={mute}
             loop={true}
-            
           />
-          </div>
-          
+
           <button
             onClick={muteHandler}
-            className={` invisible group-hover:visible text-3xl absolute text-gray-700 top-[3%] right-[7%]`}
+            className={`invisible group-hover:visible text-3xl absolute text-gray-700 top-[3%] right-[7%]`}
           >
             {!mute ? (
-              <HiMiniSpeakerWave></HiMiniSpeakerWave>
+              <HiMiniSpeakerWave />
             ) : (
-              <IoVolumeMuteSharp></IoVolumeMuteSharp>
+              <IoVolumeMuteSharp />
             )}
           </button>
         </div>
-        <div className="w-10/12 border max-md:w-full  mx-auto flex items-center justify-between py-5 ">
+        <div className="w-7/12 max-lg:w-9/12 max-md:11/12 max-sm:w-full border mx-auto flex items-center justify-between py-5">
           <div className="text-3xl w-1/4 font-extrabold text-blue-900 flex item-center">
             <button onClick={replayHandler}>
               <MdReplay />
@@ -84,7 +85,6 @@ const Study = () => {
               <FaCircleChevronRight />
             </button>
           </div>
-
           <div className="text-3xl flex justify-end items-center w-1/4 font-extrabold text-blue-900">
             <button onClick={fullScreenHandler}>
               <RiFullscreenFill />
